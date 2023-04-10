f1871a4b4e5be11ee9a9e6193a0c981a3da6dce4
haruka <haruka.1017.ishikawa@gmail.com>
2023-04-09 23:16:20 -0700

    add

---
+++ lab2/no-cra-parcel/lab/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// class App extends React.Component {
+//     render(){
+//         return(
+//             <h1>Hello World~</h1>
+//         )
+//     }
+// }
+
+const App = () => {
+    
+}
+
+const rootNode = document.getElementById('root')
+ReactDOM.render(<App />, rootNode)
